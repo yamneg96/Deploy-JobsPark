db78ad9f3e15073afb5c4f6c4f013bbfa0792b47
Yamlak Negash <164669390+yamneg96@users.noreply.github.com>
2025-09-18 16:05:20 +0300

    Update axios.js
    Updated the backend url for deployment

--- Frontend/src/services/axios.js
+++ Frontend/src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://jobspark-fdix.onrender.com/api',
   withCredentials: true, // keep this if you plan to use cookies
 });
 
